445e82d0f9345cf98ac366ac28930873d86a1e0b
Poprop <poprop10@gmail.com>
2023-10-19 17:45:30 +0300

    Update task_3.py

--- HT_02/task_3.py
+++ HT_02/task_3.py
@@ -1,23 +1,8 @@
 # Write a script which accepts a <number> from user and print
 # out a sum of the first <number> positive integers.
 
-# Не зовсім точно поставлено задачу - чи має воно отримувати одразу всі числа
-# чи потоковим введенням , тому реалізував 2 варіанти
-# __________________1 варіант через список і for з оптовою подачею чисел через кому ________________
-# x = list(map(int, input().split(",")))
-# res = 0
-# for el in x:
-#     if el != 0 and el > 0:
-#         res += el
-#     else:
-#         print(res)
-
-# __________________2 варіант через while з потоковим введенням до моменту отримання 0 чи від'ємного числа______________
-result = 0
-while True:
-    number = int(input())
-    if number != 0 and number > 0:
-        result += number
-    else:
-        print(result)
-        break
+inp_num=int(input("Please enter your number"))
+res=0
+for el in range(inp_num+1):
+    res+=el
+print(res)
