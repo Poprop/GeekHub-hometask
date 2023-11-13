5b8011defadf599f7811da908e71ed98de7ae102
Poprop <poprop10@gmail.com>
2023-11-13 17:51:04 +0200

    HT 9

--- HT_09/task_3.py
+++ HT_09/task_3.py
@@ -32,7 +32,7 @@ os.chdir(path_4_files)
 users = [["Name", "Password"],
          ["Oleg", "5623"],
          ["Ilona", "6213"],
-         ["Vasia", "5674"],
+         ["Vasia", "5555"],
          ["Dmytro", "2452"]]
 start_balance_info = [("Oleg", 25000),
                       ("Ilona", 5000),
@@ -47,6 +47,7 @@ for user, balance in start_balance_info:
         balance_txt.write(str(balance))
 jason_info = {"User_name": None,
               "Transaction_purpose": None,
+              "Amount": None,
               "Account balance": None}
 with open("transaction.json", "w", encoding="utf-8") as json_file:
     json.dump(jason_info, json_file, indent=4)
@@ -59,8 +60,12 @@ class ATM:
         self.transaction = self.load_transaction(transaction_path)
         self.current_user = None
 
-    def save_changes(self, users_file, balances_path, transactions_file):
+    def load_transaction(self, transactions_path):
+        with open(transactions_path, "r", encoding="utf-8") as transaction_json_file:
+            return json.load(transaction_json_file)
 
+    def save_changes(self, balances_path, users_file, transactions_file):
+        #
         with open(users_file, 'w', newline='', encoding='utf-8') as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerows([[user, password] for user, password in self.users.items()])
@@ -68,9 +73,15 @@ class ATM:
         for user, balance in self.balance.items():
             with open(os.path.join(balances_path, f"{user}_balance.txt"), 'w', encoding='utf-8') as balance_file:
                 balance_file.write(str(balance))
-
         with open(transactions_file, 'w', encoding='utf-8') as json_file:
             json.dump(self.transaction, json_file, indent=4)
+
+    def update_transaction(self, user, transaction_purpose, amount, balance):
+        self.transaction["User_name"] = user
+        self.transaction["Transaction_purpose"] = transaction_purpose
+        self.transaction["Amount"] = amount
+        self.transaction["Account balance"] = balance
+
     def load_users(self, user_info_path):
         with open(user_info_path, "r", encoding="utf-8") as csv_file:
             csv_reader = csv.reader(csv_file)
@@ -88,10 +99,6 @@ class ATM:
                     balances[user] = balance
         return balances
 
-    def load_transaction(self, transactions_path):
-        with open(transactions_path, "r", encoding="utf-8") as transaction_json_file:
-            return transaction_json_file
-
     def login(self, account_login: str, account_password: str):
         if account_login in self.users and self.users[account_login] == account_password:
             self.current_user = account_login
@@ -99,30 +106,32 @@ class ATM:
         else:
             print("Your enter invalid login or password")
 
-    def logout(self):
+    def logout(self, account_name):
         self.current_user = None
         print("You have logged out from the system")
 
-
-    def start(self, choice):
+    def start(self):
         while True:
-            print("Enter your action:\n"
-                  "1 - Use ATM"
-                  "2 - Exit")
-            if choice == 1:
-                username = input("enter your login: ")
+            choice = input("1 - Use ATM\n"
+                           "2 - Exit\n"
+                           "Enter your action: ")
+
+            if choice == "1":
+
+                user_name_function = input("enter your login: ").strip().capitalize()
                 password = input("enter your password: ")
-                self.login(username, password)
+                self.login(user_name_function, password)
                 if self.current_user:
-                    self.menu()
+                    self.menu(user_name_function)
                     break
-            if choice == 2:
-                print("Good bye , see you next time")
+            if choice == "2":
+                print("Good bye , see    you next time")
                 break
             else:
                 print("Your choice is incorrect , you should enter 1 (for using ATM) or 2 ( for exit)")
 
-    def menu(self):
+    def menu(self, user_name_function):
+        user = user_name_function
         while self.current_user:
             menu_options = {
                 '1': 'check_balance',
@@ -135,9 +144,11 @@ class ATM:
                 print(f"{key} <-------- {value}")
             choice = input("Your choice is : ")
             if choice in menu_options:
-                getattr(self, menu_options[choice])
+                getattr(self, menu_options[choice])(account_name=user)
             else:
                 print("Invalid option , try again")
+            if not self.current_user:
+                break
 
     def add_account(self, users_name: str, user_password: str):
         if users_name not in self.users:
@@ -149,24 +160,32 @@ class ATM:
 
     def check_balance(self, account_name: str):
         if account_name in self.balance:
-            return self.balance[account_name]
+            print(self.balance[account_name])
         else:
             print(f"Account {account_name} not exists")
 
-    def deposit(self, account_name: str, amount: str):
+    def deposit(self, account_name: str):
         if account_name in self.balance:
+            amount = int(input("Enter please amount of deposit: "))
             self.balance[account_name] += amount
             print(f"On account {account_name} was successfully deposited {amount} UAH ,\n"
                   f"curent deposite is {self.balance[account_name]}")
+            self.update_transaction(user=account_name, transaction_purpose="Deposit", amount=amount, balance=balance)
+            self.save_changes(user_balance_path, users_info_path, users_transactions_path)
         else:
             print(f"Account {account_name} is not exists")
 
-    def withdraw(self, account_name: str, amount):
+    def withdraw(self, account_name: str):
         if account_name in self.balance:
+            amount = int(input("Enter amount of withdraw: "))
             if self.balance[account_name] >= amount:
                 self.balance[account_name] -= amount
                 print(f"From account {account_name} was successfully withrawed {amount} UAH ,\n"
                       f"current deposit is {self.balance[account_name]}")
+                self.update_transaction(user=account_name, transaction_purpose="Withdraw", amount=amount,
+                                        balance=balance)
+                self.save_changes(user_balance_path, users_info_path, users_transactions_path)
+
             else:
                 print(f"There is not enough money on your balance")
 
@@ -174,5 +193,5 @@ class ATM:
 users_info_path = f"{path_4_files}\\Names&passwords.csv"
 user_balance_path = f"{path_4_files}"
 users_transactions_path = f"{path_4_files}\\transaction.json"
-atm = ATM(users_info_path, user_balance_path, users_transactions_path)
-atm.start(1)
+atm = ATM(user_balance_path, users_info_path, users_transactions_path)
+atm.start()
