203b8e35584cf834a2d2db2e7bfa690de1f4dbdf
Poprop <poprop10@gmail.com>
2023-11-30 22:55:57 +0200

    HT 13 task 4

--- HT_13/task_4.py
+++ HT_13/task_4.py
@@ -3,34 +3,24 @@
 але індексація повинна починатись із 1"""
 
 
-class CustomList:
-    def __init__(self, *args):
-        self._data = list(args)
+class CustomList(list):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, **kwargs)
 
     def __getitem__(self, index):
         if index < 1:
             raise IndexError("Index must be greater than or equal to 1")
-        return self._data[index - 1]
+        return super().__getitem__(index - 1)
 
     def __setitem__(self, index, value):
         if index < 1:
             raise IndexError("Index must be greater than or equal to 1")
-        while len(self._data) < index:
-            self._data.append(None)
-        self._data[index - 1] = value
-
-    def __len__(self):
-        return len(self._data)
-
-    def __repr__(self):
-        return repr(self._data)
+        while len(self) < index:
+            self.append(None)
+        super().__setitem__(index - 1, value)
 
 
 if __name__ == "__main__":
     list_1 = CustomList(10, 15, 20, 12, 15)
-    print(f"Original list {list_1}")
-    print(f"{list_1[1]}")
-    try:
-        list_1[0] = 1
-    except IndexError as _:
-        print("In custom list doesnt exist 0 index")
+    list_1[0] = 5
+    print(list_1)
